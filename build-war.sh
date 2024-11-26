7af4651e81411e932f3c1da9c0551c53e78d7259
Ratecki <scarabeo@poczta.fm>
2024-11-26 13:39:33 +0100

    add configuration to war package

--- build-war.sh
+++ build-war.sh
@@ -4,7 +4,7 @@ echo "### Create helloworld.war file"
 echo
 echo "Calling jar"
 echo
-jar -cvf helloworld.war *.jsp WEB-INF
+jar -cvf helloworld.war *.jsp WEB-INF configuration.yaml
 echo
 echo "Moving helloworld.war to dist/ with option --FORCE"
 mv -f helloworld.war dist/
